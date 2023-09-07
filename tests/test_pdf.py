1cff774dc5d48169cc835172fa90590ca3706455
Ramil Sayapov <ramilsayapov71@gmail.com>
2023-09-07 23:27:46 +0700

    working_3

--- tests/test_pdf.py
+++ tests/test_pdf.py
@@ -1,15 +1,21 @@
-import pypdf
+import pypdf, os
+from conftest import RESOURCES_DIR
 # TODO оформить в тест, добавить ассерты и использовать универсальный путь
-reader = pypdf.PdfReader('resources/docs-pytest-org-en-latest.pdf')
-number_of_pages = len(reader.pages)
-first_page = reader.pages[0]
-text = first_page.extract_text()
-# print(number_of_pages)
-# print(first_page)
-print(text)
-count = 0
-for image_file in first_page.images:
-    with open(str(count) + image_file.name, 'wb') as fp:
-        fp.write(image_file.data)
-        count += 1
+
+def test_pdf():
+    PDF_FILE_1 = os.path.join(RESOURCES_DIR, 'docs-pytest-org-en-latest.pdf')
+    reader = pypdf.PdfReader(PDF_FILE_1)
+    number_of_pages = len(reader.pages)
+    first_page = reader.pages[0]
+    text = first_page.extract_text()
+    print(number_of_pages)
+    # print(first_page)
+    print(text)
+    count = 0
+    for image_file in first_page.images:
+        with open(os.path.join(RESOURCES_DIR, str(count) + image_file.name), 'wb') as fp:
+            fp.write(image_file.data)
+            count += 1
+
+    assert number_of_pages == 412
 
