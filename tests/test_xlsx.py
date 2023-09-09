ffafb774c0da78a77e1fadf75db22f454bc2177c
Ramil Sayapov <ramilsayapov71@gmail.com>
2023-09-09 21:31:48 +0700

    working_5

--- tests/test_xlsx.py
+++ tests/test_xlsx.py
@@ -1,6 +1,11 @@
 from openpyxl import load_workbook
+import os
+from tests.conftest import RESOURCES_DIR
 # TODO оформить в тест, добавить ассерты и использовать универсальный путь
 
-workbook = load_workbook('resources/file_example_XLSX_50.xlsx')
-sheet = workbook.active
-print(sheet.cell(row=3, column=2).value)
+def test_xlsx():
+    workbook = load_workbook(os.path.join(RESOURCES_DIR, 'file_example_XLSX_50.xlsx'))
+    sheet = workbook.active
+    print(sheet.cell(row=3, column=2).value)
+
+    #assert
