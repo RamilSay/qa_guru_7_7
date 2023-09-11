e00f239c8c843f05ea14cda34002f72961493c46
Ramil Sayapov <ramilsayapov71@gmail.com>
2023-09-11 15:00:47 +0700

    working_6

--- tests/test_zip.py
+++ tests/test_zip.py
@@ -1,9 +1,15 @@
 import zipfile, os
 from tests.conftest import RESOURCES_DIR
 
+files_to_zip = os.listdir(RESOURCES_DIR)
 def test_zip():
-    archive_zip = os.path.join(RESOURCES_DIR, 'tmp.zip')
-    with zipfile.ZipFile(archive_zip, 'r') as zip_file:
-        print(f'Содержимое архива: {zip_file.namelist()}')
+    with zipfile.ZipFile(os.path.join(RESOURCES_DIR, 'tmp.zip'), 'w') as zip_file:
+        for file_to_zip in files_to_zip:
+            file_path = os.path.join(RESOURCES_DIR, file_to_zip)
+            zip_file.write(file_path, file_to_zip)
+        name_list = zip_file.namelist()
+        print(f'Содержимое архива: {name_list}')
 
+    assert name_list == files_to_zip
 
+    os.remove(os.path.join(RESOURCES_DIR, 'tmp.zip'))
